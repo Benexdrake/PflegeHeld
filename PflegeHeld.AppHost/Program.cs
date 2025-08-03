bb4bc0a90ee456f1aa21bbd099af0af24ac0eb05
BeneX <BeneX@hotmail.de>
2025-08-03 12:22:49 +0200

    feat: add sql server

--- PflegeHeld.AppHost/Program.cs
+++ PflegeHeld.AppHost/Program.cs
@@ -1,6 +1,11 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.PflegeHeld_Backend_API_Personal>("pflegeheld-backend-api-personal");
+var sqlServerPassword = builder.AddParameter("sqlPassword", secret: true, value: "P@ssword1");
+
+var sql = builder.AddSqlServer("gamedevsconnect-backend-sql", port: 1400, password: sqlServerPassword)
+                 .WithVolume("pflegeheld-sqlserver-data", "/var/opt/mssql");
+
+//builder.AddProject<Projects.PflegeHeld_Backend_API_Personal>("pflegeheld-backend-api-personal");
 
 var build = builder.Build();
 
